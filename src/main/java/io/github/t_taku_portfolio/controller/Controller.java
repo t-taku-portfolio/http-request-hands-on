9ab0ea488bb2e05bfa9776c8bcc9f7cfb5d55849
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-17 10:54:13 +0900

    chore(controller): create a controller interface

---
+++ src/main/java/io/github/t_taku_portfolio/controller/Controller.java
@@ -0,0 +1,10 @@
+package io.github.t_taku_portfolio.controller;
+
+import com.sun.net.httpserver.HttpExchange;
+import io.github.t_taku_portfolio.model.StudentRequestDTO;
+
+import java.io.IOException;
+
+public interface Controller {
+    public void handle(HttpExchange httpExchange, StudentRequestDTO dto) throws IOException;
+}
--- src/main/java/io/github/t_taku_portfolio/controller/StudentsController.java
+++ src/main/java/io/github/t_taku_portfolio/controller/StudentsController.java
@@ -1,5 +1,15 @@
 package io.github.t_taku_portfolio.controller;
 
+import com.sun.net.httpserver.HttpExchange;
+
 public class StudentsController {
+    HttpExchange httpExchange;
+
+    //
+    // accept HttpExchange instance from the Dispatcher in constructor
+    //
+    StudentsController(HttpExchange theHttpExchange){
+        this.httpExchange = theHttpExchange;
+    }
 
 }
