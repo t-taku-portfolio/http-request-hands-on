8f405c02cd750a659ff48f229703c9eca8782c7b
Taku <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-18 23:58:55 +0900

    Feature (#5)
    * docs(README): Update README
    
    * refctor(StudentController): make controller singleton

--- src/main/java/io/github/t_taku_portfolio/controller/StudentsController.java
+++ src/main/java/io/github/t_taku_portfolio/controller/StudentsController.java
@@ -2,10 +2,18 @@ package io.github.t_taku_portfolio.controller;
 
 import com.sun.net.httpserver.HttpExchange;
 import io.github.t_taku_portfolio.model.RequestDTO;
+import io.github.t_taku_portfolio.service.Service;
 
 import java.io.IOException;
 
 public class StudentsController implements Controller {
+    private static StudentsController instance;
+    private final Service service;
+
+    private StudentsController(Service theService) {
+        this.service = theService;
+    }
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         // business logic here
@@ -20,5 +28,13 @@ public class StudentsController implements Controller {
                 "contentType",
                 "path"
         );
+    }
+
+    public static void initialize(Service theService) {
+        if(instance == null) instance = new StudentsController(theService);
+    }
+
+    public static StudentsController getInstance() {
+        return instance;
     }
 }
