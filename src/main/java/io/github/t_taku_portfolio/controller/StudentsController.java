926bfaedbd7e324b64a522408b60053b587eb1b9
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-18 23:00:06 +0900

    chore(JsonUtil): create JsonUtil class that manage object mapper instance

--- src/main/java/io/github/t_taku_portfolio/controller/StudentsController.java
+++ src/main/java/io/github/t_taku_portfolio/controller/StudentsController.java
@@ -2,6 +2,7 @@ package io.github.t_taku_portfolio.controller;
 
 import com.sun.net.httpserver.HttpExchange;
 import io.github.t_taku_portfolio.model.StudentRequestDTO;
+import tools.jackson.core.json.JsonFactory;
 
 import java.io.IOException;
 
@@ -9,5 +10,16 @@ public class StudentsController implements Controller {
     @Override
     public void handle(HttpExchange httpExchange, StudentRequestDTO dto) throws IOException {
         // business logic here
+
+        // extract the request body as a JSON file using Jackson library
+
+
+
+        // store the data from client into DTO
+        StudentRequestDTO dto1 = new StudentRequestDTO(
+                "method",
+                "contentType",
+                "path"
+        );
     }
 }
