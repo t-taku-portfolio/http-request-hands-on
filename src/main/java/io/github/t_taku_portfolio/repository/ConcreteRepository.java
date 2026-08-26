0dc322e4f6f8e8433820b1d3bf70a413ea7910eb
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-26 19:15:16 +0900

    chore(validtr, repo): correct regex and change arg
    correct regex in validator
    change arg of the repository's save method'

--- src/main/java/io/github/t_taku_portfolio/repository/ConcreteRepository.java
+++ src/main/java/io/github/t_taku_portfolio/repository/ConcreteRepository.java
@@ -1,17 +1,19 @@
 package io.github.t_taku_portfolio.repository;
 
 
+import io.github.t_taku_portfolio.model.StudentBodyDTO;
+
 public class ConcreteRepository implements Repository{
     public  ConcreteRepository() {
         System.out.println("in constructor: " + getClass().getSimpleName());
     }
 
-    public boolean save(String name, int grade) {
+    public boolean save(StudentBodyDTO dto) {
         int rows = 0;
 
         JDBCExecutor executor = new JDBCExecutor();
         try{
-            rows = executor.query(name, grade);
+            rows = executor.query(dto.name(), dto.year());
         } catch(Exception e){
             System.err.println("Error: " + e.getMessage());
         }
