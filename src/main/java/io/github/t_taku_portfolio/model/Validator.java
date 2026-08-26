f19ae1bb536fa43167cfd958b5b1ec9abf10d95d
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-27 00:18:49 +0900

    chore(validator): correct validator

--- src/main/java/io/github/t_taku_portfolio/model/Validator.java
+++ src/main/java/io/github/t_taku_portfolio/model/Validator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Validator {
     // Hiragana : \\u3040-\\u309f, Katakana : \\u30A0-\\u30FF, Kanji : \\u4E00-\\u9FAF
     // Currently allow alphanumeric input only to simplify the configurations of each layer
-    private static final Pattern WHITELIST_PATTERN = Pattern.compile("[^a-zA-Z0-9\\s]+$");
+    private static final Pattern WHITELIST_PATTERN = Pattern.compile("^[a-zA-Z0-9\\s]+$");
 
     public static void validatePath(String path) {
         if(path == null) {
