81163f0ccf73a112c655d90aabf96f580d95ca51
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-27 01:04:52 +0900

    chore(SQLstatement): correct the name to the table name

--- src/main/java/io/github/t_taku_portfolio/repository/Repository.java
+++ src/main/java/io/github/t_taku_portfolio/repository/Repository.java
@@ -1,4 +1,7 @@
 package io.github.t_taku_portfolio.repository;
 
+import io.github.t_taku_portfolio.model.StudentBodyDTO;
+
 public interface Repository {
+    boolean save(StudentBodyDTO dto);
 }
--- src/main/java/io/github/t_taku_portfolio/repository/SQLStatement.java
+++ src/main/java/io/github/t_taku_portfolio/repository/SQLStatement.java
@@ -1,5 +1,5 @@
 package io.github.t_taku_portfolio.repository;
 
 public class SQLStatement {
-    public static final String INSERT = "INSERT INTO student (name, grade) VALUES (?, ?)";
+    public static final String INSERT = "INSERT INTO students (name, grade) VALUES (?, ?)";
 }
