180165d500a8de3119ff75e3483623c6e6034f65
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-26 19:22:41 +0900

    refactor(service): call repo layer from service

--- src/main/java/io/github/t_taku_portfolio/service/ServiceImp.java
+++ src/main/java/io/github/t_taku_portfolio/service/ServiceImp.java
@@ -1,18 +1,26 @@
 package io.github.t_taku_portfolio.service;
 
 import io.github.t_taku_portfolio.model.StudentBodyDTO;
+import io.github.t_taku_portfolio.repository.ConcreteRepository;
 
 public class ServiceImp {
     StudentBodyDTO studentBodyDTO;
+    ConcreteRepository concreteRepository;
 
     public void setStudentBodyDTO(StudentBodyDTO studentBodyDTO) {
         this.studentBodyDTO = studentBodyDTO;
+
     }
 
     public void doSomething(){
+        // toggle methods based on request method
+        // currently single method only exists
+        saveStudent();
     }
 
-    void saveStudent(){}
+    void saveStudent(){
+        concreteRepository.save(studentBodyDTO);
+    }
 
     StudentBodyDTO readStudent(){
         return null;
