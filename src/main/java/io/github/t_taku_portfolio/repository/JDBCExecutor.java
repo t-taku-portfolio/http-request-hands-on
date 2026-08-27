89d741bc7cdf7475602c59020963bffa532ae3ef
Taku <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-27 10:17:11 +0900

    chore(service, repo): correct broken logics and, add JDBC driver for PostgreSQL (#10)
    * refactor(service): call repo layer from service
    
    * chore(validator): correct validator
    
    * refactor(service): correct broken di
    
    * refactor(service): call own class name from construcor
    
    * chore(example.env): correct env sample
    
    * build(pom): add dependency postgresql jdbc driver
    
    * chore(executor): correct execution logic
    
    * chore(SQLstatement): correct the name to the table name
    
    * chore(executor): add validation for environment variables
    
    * refactor(controller): check result of saving student
    
    * docs(readme): update roadmap

--- src/main/java/io/github/t_taku_portfolio/repository/JDBCExecutor.java
+++ src/main/java/io/github/t_taku_portfolio/repository/JDBCExecutor.java
@@ -15,13 +15,16 @@ public class JDBCExecutor {
         this.dbPath = System.getenv("DB_DIR");
         this.username = System.getenv("DB_USER");
         this.password = System.getenv("DB_PASSWORD");
+
+        if (this.dbPath == null || this.username == null || this.password == null) {
+            throw new IllegalStateException("Environment variables missing");
+        }
     }
 
     public int query(String name, int year) throws SQLException {
         try (Connection connection = DriverManager.getConnection(dbPath,username,password);
              PreparedStatement preparedStatement = connection.prepareStatement(SQLStatement.INSERT)
         ) {
-            connection.setAutoCommit(false);
             preparedStatement.setString(1, name);
             preparedStatement.setInt(2, year);
 
--- src/main/java/io/github/t_taku_portfolio/service/ServiceImp.java
+++ src/main/java/io/github/t_taku_portfolio/service/ServiceImp.java
@@ -1,18 +1,34 @@
 package io.github.t_taku_portfolio.service;
 
 import io.github.t_taku_portfolio.model.StudentBodyDTO;
+import io.github.t_taku_portfolio.repository.ConcreteRepository;
 
 public class ServiceImp {
     StudentBodyDTO studentBodyDTO;
+    ConcreteRepository concreteRepository;
+
+    public ServiceImp(ConcreteRepository concreteRepository) {
+        this.concreteRepository = concreteRepository;
+        System.out.println("in constructor: " + getClass().getSimpleName());
+    }
 
     public void setStudentBodyDTO(StudentBodyDTO studentBodyDTO) {
         this.studentBodyDTO = studentBodyDTO;
     }
 
-    public void doSomething(){
+    public boolean doSomething(){
+        // toggle methods based on request method
+        // currently single method only exists
+        if(!saveStudent()) {
+            System.out.println("save failed");
+            return false;
+        }
+        return true;
     }
 
-    void saveStudent(){}
+    boolean saveStudent(){
+        return concreteRepository.save(studentBodyDTO);
+    }
 
     StudentBodyDTO readStudent(){
         return null;
