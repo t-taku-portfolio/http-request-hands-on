18b94f59cd31167f3659e108f58edc64056f33eb
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-28 11:02:01 +0900

    chore(test): create a dispatcher test skelton

---
+++ src/test/java/io/github/t_taku_portfolio/controller/DispatcherTest.java
@@ -0,0 +1,12 @@
+package io.github.t_taku_portfolio.controller;
+
+public class DispatcherTest {
+    public void happyPathTest(){
+    }
+
+    public void edgeCaseTest() {
+    }
+
+    public void failureCaseTest() {
+    }
+}
