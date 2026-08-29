c7aadc0c22e81cc7756b30450b501e5b792251e5
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-29 19:46:14 +0900

    chore(test): create mock router and controller

--- src/test/java/io/github/t_taku_portfolio/controller/DispatcherTest.java
+++ src/test/java/io/github/t_taku_portfolio/controller/DispatcherTest.java
@@ -1,7 +1,19 @@
 package io.github.t_taku_portfolio.controller;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
 public class DispatcherTest {
+
+    @Test
     public void normalCaseTest() {
+        Router mockRouter = Mockito.mock(Router.class);
+        Controller mockController = Mockito.mock(Controller.class);
+
+        Mockito.when(mockRouter.resolve("/students"))
+                .thenReturn(Optional.of(() -> mockController));
     }
 
     public void edgeCaseTest() {
