3825884d847cbd2c9f71106fc6cd93f3acd2935d
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-08-31 01:06:35 +0900

    test(controller): verify the handle method of controller

--- src/test/java/io/github/t_taku_portfolio/controller/DispatcherTest.java
+++ src/test/java/io/github/t_taku_portfolio/controller/DispatcherTest.java
@@ -1,19 +1,36 @@
 package io.github.t_taku_portfolio.controller;
 
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.Optional;
 
 public class DispatcherTest {
 
     @Test
-    public void normalCaseTest() {
+    public void normalCaseTest() throws IOException {
         Router mockRouter = Mockito.mock(Router.class);
         Controller mockController = Mockito.mock(Controller.class);
 
         Mockito.when(mockRouter.resolve("/students"))
                 .thenReturn(Optional.of(() -> mockController));
+
+        Dispatcher dispatcher = new Dispatcher(mockRouter);
+
+        Headers mockHeaders = Mockito.mock(Headers.class);
+        Mockito.when(mockHeaders.getFirst("Content-Type")).thenReturn("application/json");
+
+        HttpExchange mockExchange = Mockito.mock(HttpExchange.class);
+        Mockito.when(mockExchange.getRequestMethod()).thenReturn("GET");
+        Mockito.when(mockExchange.getRequestURI()).thenReturn(URI.create("/students"));
+        Mockito.when(mockExchange.getRequestHeaders()).thenReturn(mockHeaders);
+
+        dispatcher.handle(mockExchange);
+        Mockito.verify(mockController).handle(mockExchange);
     }
 
     public void edgeCaseTest() {
