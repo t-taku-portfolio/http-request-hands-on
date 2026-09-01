76851f19ad795bba47f22b8258839722394a67f6
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-09-02 00:03:08 +0900

    chore(test): rewrite failCase

--- src/test/java/io/github/t_taku_portfolio/model/StudentBodyDTOTest.java
+++ src/test/java/io/github/t_taku_portfolio/model/StudentBodyDTOTest.java
@@ -2,25 +2,25 @@ package io.github.t_taku_portfolio.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StudentBodyDTOTest {
     @Test
     public void normalCase() {
         // Check if the method works with the happy path
-        StudentBodyDTO studentBodyDTOtest = new StudentBodyDTO(
-                "name", 4);
+        StudentBodyDTO studentBodyDTOtest1 = new StudentBodyDTO("taro", 4);
+        StudentBodyDTO studentBodyDTOtest2 = new StudentBodyDTO("hanako", 1);
+        StudentBodyDTO studentBodyDTOtest3 = new StudentBodyDTO("jiro", 2);
     }
+
     @Test
     public void edgeCase1() {
         // Check if the method works with the boundary arguments
-        StudentBodyDTO studentBodyDTOtest1 = new StudentBodyDTO(
-                "name", 1
-        );
-
-        StudentBodyDTO studentBodyDTOtest2 = new StudentBodyDTO(
-                "name", 6
-        );
+        StudentBodyDTO studentBodyDTOtest1 = new StudentBodyDTO("jim", 1);
+        StudentBodyDTO studentBodyDTOtest2 = new StudentBodyDTO("mike", 6);
+        StudentBodyDTO studentBodyDTOtest3 = new StudentBodyDTO("curly", 1);
     }
 
     @Test
@@ -28,14 +28,11 @@ public class StudentBodyDTOTest {
         // Check if the method throws expected exception
         // Specify the expected exception and function as arguments
 
-        Throwable exception1 = assertThrows(
-                IllegalArgumentException.class,
-                () -> new StudentBodyDTO("name", 0)
-        );
-
-        Throwable exception2 = assertThrows(
-                IllegalArgumentException.class,
-                () -> new StudentBodyDTO("name", 7)
-        );
+        Map<String, Integer> students = Map.of("taro", -1, "hanako", 7, "太郎", 1);
+        for (Map.Entry<String, Integer> entry : students.entrySet()) {
+            Throwable exception = assertThrows(
+                    IllegalArgumentException.class,
+                    () -> new StudentBodyDTO(entry.getKey(), entry.getValue()));
+        }
     }
 }
