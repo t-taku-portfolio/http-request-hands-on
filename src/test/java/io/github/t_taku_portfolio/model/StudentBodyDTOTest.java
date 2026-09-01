8bacebcea99ef6930bb64d55cd00e88c93fad138
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-09-01 11:35:28 +0900

    test(model): create a fail case test for dto with Junit

--- src/test/java/io/github/t_taku_portfolio/model/StudentBodyDTOTest.java
+++ src/test/java/io/github/t_taku_portfolio/model/StudentBodyDTOTest.java
@@ -2,14 +2,18 @@ package io.github.t_taku_portfolio.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class StudentBodyDTOTest {
     @Test
     public void normalCase() {
+        // Check if the method works with the happy path
         StudentBodyDTO studentBodyDTOtest = new StudentBodyDTO(
                 "name", 4);
     }
     @Test
     public void edgeCase1() {
+        // Check if the method works with the boundary arguments
         StudentBodyDTO studentBodyDTOtest1 = new StudentBodyDTO(
                 "name", 1
         );
@@ -19,5 +23,19 @@ public class StudentBodyDTOTest {
         );
     }
 
-    public void failCase() {}
+    @Test
+    public void failCase() {
+        // Check if the method throws expected exception
+        // Specify the expected exception and function as arguments
+
+        Throwable exception1 = assertThrows(
+                IllegalArgumentException.class,
+                () -> new StudentBodyDTO("name", 0)
+        );
+
+        Throwable exception2 = assertThrows(
+                IllegalArgumentException.class,
+                () -> new StudentBodyDTO("name", 7)
+        );
+    }
 }
